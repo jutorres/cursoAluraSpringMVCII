deef61cfac0da349aa8259d47181fc979f7d82fd
jutorres <torresjujuliana@gmail.com>
2017-08-23 22:35:53 -0400

    Class 5 completed

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
@@ -28,9 +29,10 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(Produto produto) {
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
 		produtoDAO.gravar(produto);
-		return "produtos/ok";
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		return new ModelAndView("redirect:produtos");
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
