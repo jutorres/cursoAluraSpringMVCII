66a746222c34926ab8dc22d8ca732094d18596f9
jutorres <torresjujuliana@gmail.com>
2017-08-29 16:26:12 -0400

    Class 7 completed

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -12,13 +14,24 @@ import br.com.casadocodigo.loja.daos.ProdutoDAO;
 @ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class })
 
 public class AppWebConfiguration {
-	
+
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
-		 InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		 resolver.setPrefix("/WEB-INF/views/");
-		 resolver.setSuffix(".jsp");
-		 return resolver;
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(1);
+
+		return messageSource;
 	}
 
 }
